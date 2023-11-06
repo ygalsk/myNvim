276332af3db47eb678de497b032ef7fd48392d07
Daniel Kremer <dkremer@2-F-6.42heilbronn.de>
2023-11-07 00:22:01 +0100

    test

--- lua/myls/plugins/42Header.lua
+++ lua/myls/plugins/42Header.lua
@@ -3,14 +3,4 @@ return {
         "42Paris/42header",
         event = "InsertEnter",
     } --,
-		-- opts = function ()
-			-- local header = require("42header")
-
-			-- header.setup({
-				-- name = {
-					-- user42 = vim.g.user42,
-					-- mail42 = vim.g.mail42,
-			-- },
-		-- })
-		-- end,
 }
