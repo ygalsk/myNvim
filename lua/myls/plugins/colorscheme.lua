f9bf3be67d6a53790e0fca403f6f9ff61b9ab949
Daniel Kremer <dkremer@2-F-6.42heilbronn.de>
2023-11-07 00:41:24 +0100

    gooooo

--- lua/myls/plugins/colorscheme.lua
+++ lua/myls/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
 
---TokyoNight/Tokyonight_Storm Theme
+-- TokyoNight/Tokyonight_Storm Theme
 
---"folke/tokyonight.nvim",
---lazy = false,
---priority = 888,
---config = function()
-	--vim.cmd([[colorscheme tokyonight-storm]])
---end,
---}
+-- "folke/tokyonight.nvim",
+-- lazy = false,
+-- priority = 888,
+-- config = function()
+-- 	vim.cmd([[colorscheme tokyonight-storm]])
+-- end,
+-- }
 
 --NightFox/CarbonFox Theme
-
+--
 "EdenEast/nightfox.nvim",
 lazy = false,
 priority = 888,
