8dea4147e4decc7b25597861d55dab7612bda662
dkremer <daniel.kremer@hotmail.de>
2024-07-14 02:03:18 +0200

    some fun

--- lua/myls/plugins/colorscheme.lua
+++ lua/myls/plugins/colorscheme.lua
@@ -1,1 +1,17 @@
-return { "catppuccin/nvim", name = "catppuccin-mocha", priority = 1000 }
+return {
+    "catppuccin/nvim",
+    name = "catppuccin-mocha",
+    priority = 1000,
+		config = function()
+    require("catppuccin").setup({
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            treesitter = true,
+            notify = true,
+						neotree = true,
+        },
+    })
+end,
+}
